39a35925cf6f8b308ab445f2c6a7eb6a83084cd4
Gavriil84 <gabwt.84@gmail.com>
2021-10-02 15:33:58 -0700

    add js functions

--- main.js
+++ main.js
@@ -3,28 +3,37 @@ const food = [];
 class Foods {
     constructor(name, carb, fat, protein) {
         this.name = name;
-        this.carb = carb;
-        this.fat = fat;
-        this.protein = protein;
+        this.carb = parseFloat(carb).toFixed(2);
+        this.fat = parseFloat(fat).toFixed(2);
+        this.protein = parseFloat(protein).toFixed(2);
     }
 }
 
 const btn = document.getElementById('btn');
 
-const foodSum = function() {    
-    let foodDetail = new Foods(foodName.value, carb.value, fat.value, protein.value)
-    food.push(foodDetail);
+const foodSum = function() {
+    const name = document.getElementById('foodName').value;
+    const carb = document.getElementById('carbs').value;
+    const fat = document.getElementById('fats').value;
+    const protein = document.getElementById('proteins').value;
 
+
+    let foodDetail = new Foods(name, carb, fat, protein)
+
+    if (name == null || name == "", carb == null || carb == "", fat == null || fat == "", protein == null || protein == "") {
+        alert("Please Fill All Required Field");
+        return false;
+    } else {
+        food.push(foodDetail);
+    }
+
+    
     foodName.value = "";
-    carb.value = "";
-    fat.value = "";
-    protein.value = "";
+    carbs.value = "";
+    fats.value = "";
+    proteins.value = "";
 }
 
-
 btn.addEventListener('click', foodSum);
-
-
-
 
 console.log(food)
